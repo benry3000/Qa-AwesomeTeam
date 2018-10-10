ce0123dba67c9d4c20cb987b022078efc3cab7e8
Benry Dong <benry.dong@ad.infosys.com>
2018-10-09 20:06:47 -0400

    added more testcase

--- ZEROBANK_POMFramework/src/test/java/com/zero/qa/testcases/OnlineBankingTest.java
+++ ZEROBANK_POMFramework/src/test/java/com/zero/qa/testcases/OnlineBankingTest.java
@@ -6,43 +6,29 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.zero.qa.base.TestBase;
-import com.zero.qa.pages.FeedBack;
 import com.zero.qa.pages.Home;
 import com.zero.qa.pages.OnlineBanking;
 
-public class HomeTest extends TestBase{
+public class OnlineBankingTest extends TestBase {
 	
-	Home home;
-	OnlineBanking ob;
-	public HomeTest() {
+	OnlineBanking onlinebanking;
+	
+	public OnlineBankingTest() {
 		super();
 	}
 	
 	@BeforeMethod
 	public void setUp() {
 		initialization();
-		home = new Home();
-		ob = new OnlineBanking();
+		onlinebanking = new OnlineBanking();
 	}
 	
 	@Test(priority=1) //title
 	public void validateTitle() {
-		String title = home.validateHomeTitle();
-		Assert.assertEquals(title, "Zero - Personal Banking - Loans - Credit Cards");
-		
-	}
-	
-	@Test(priority=2) //Click Online Banking
-	public void validateOnlineBanking() {
-		home.clickOnOnlineBanking();
-		String title = ob.validateOnlineBankingTitle();
+		String title = onlinebanking.validateOnlineBankingTitle();
 		Assert.assertEquals(title, "Zero - Free Access to Online Banking");
 		
 	}
-	
-	
-	
-
 	
 	
 	@AfterMethod
